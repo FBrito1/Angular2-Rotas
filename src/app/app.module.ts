ab58b9093c838eab8d2a1aef75ccf51ebe32d089
Fabio Brito <luan.tecno@gmail.com>
2017-10-04 16:06:19 -0300

    redirecionamento de rotas e criação do componente curso-nao-encontrado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
+import { CursosService } from './cursos/cursos.service';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     routing
   ],
-  providers: [],
+  providers: [
+    CursosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 const APP_ROUTES: Routes = [
     { path: 'cursos', component: CursosComponent},
+    { path: 'curso/:id', component: CursoDetalheComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'notFound', component: CursoNaoEncontradoComponent },
     { path: '', component: HomeComponent }
 ]
 
