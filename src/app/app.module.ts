abce2ef0d07eeb3031c038fe91e47c91f00f26bf
Fabio Brito <luan.tecno@gmail.com>
2017-10-06 11:30:08 -0300

    refatoração do código com a criação do modulo cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//import { routing } from './app.routing';
+
+import { CursosModule } from './cursos/cursos.module';
 import { AppRoutingModule } from './app.routing.module';
 
 import { MaterializeModule } from 'angular2-materialize';
@@ -10,29 +11,21 @@ import { MaterializeModule } from 'angular2-materialize';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    CursosModule,
     AppRoutingModule
-    //routing
+  
   ],
-  providers: [
-    CursosService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
